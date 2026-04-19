098ca7840c74e0f6c39fcd0796b102c0ef64e4a5
HoangManhh <nhmanh4work@gmail.com>
2026-04-19 17:45:21 +0700

    Fix: Hardcode firebase config directly to fix Syntax Error

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCuHnA_lhqRd_Jf4_YYWxA0A1_Og6pRshk",
+  authDomain: "traditional-game-6e78a.firebaseapp.com",
+  databaseURL: "https://traditional-game-6e78a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "traditional-game-6e78a",
+  storageBucket: "traditional-game-6e78a.firebasestorage.app",
+  messagingSenderId: "187333146201",
+  appId: "1:187333146201:web:8e06a7a47c4b1f30d16348"
+};
